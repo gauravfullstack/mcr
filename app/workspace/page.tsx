eb84f8e6b7d3114054a44b03284e6650f8d4b762
Gaurav <gauravtiwari29500@gmail.com>
2026-09-23 08:27:32 +0530

    First Commit todo app 23-09 8:00

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -3,18 +3,8 @@ import style from './parent.module.css'
 export default function Workspace() {
   return (
     <div className={style.container}>
-      <div className={style.todo_container}>
-        <h1>Todo App</h1>
-        <div>
-          <input
-            type="text"
-            placeholder='Enter your todo here...'
-            className={style.todo_input}
-          />
-          <button>Add</button>
-        </div>
-
-      </div>
+      <h1 className={style.title}>Todo App</h1>
+      
     </div>
   );
 }
