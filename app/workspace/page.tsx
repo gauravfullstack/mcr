49ca021cfe8da6c3f8982ba4d3141dfaf0703907
Gaurav Tiwari <gaurav.tiwari@praxis-services.com>
2026-09-23 10:51:26 +0530

    ui done

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,13 +1,9 @@
-import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList';
 import style from './parent.module.css'
 
 export default function Workspace() {
   return (
     <div className={style.container}>
-      <h1 className={style.title}>Todo App</h1>
-      <TodoInput />
-      <TodoList />
+      <h1 className={style.title}>Dummy App</h1>
     </div>
   );
 }
